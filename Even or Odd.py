21e63fb913c71ea28dbc42543fca4c3985ec997a
ASDFGH-ALEX <alexsabu053@gmail.com>
2024-11-29 18:19:08 +0530

    Add files via upload

---
+++ Even or Odd.py
@@ -0,0 +1,12 @@
+'''
+Author:Alex
+Date:29-11-2024
+Description:Write a Python program that prompts the user to enter a number and
+checks if the number is even or odd. If the number is even, print "The
+number is even." If the number is odd, print "The number is odd
+'''
+num_1=int(input("Enter a number: "))
+if num_1%2==0:
+    print("The number is even")
+else:
+    print("The number is odd")
