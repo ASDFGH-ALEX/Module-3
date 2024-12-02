6b3e20c044b2cf6d2286f0f636d208d335db9544
ASDFGH-ALEX <alexsabu053@gmail.com>
2024-12-02 10:05:55 +0530

    Add files via upload

---
+++ Sample.py
@@ -0,0 +1,35 @@
+'''
+digits=list(range(10))
+print(digits)
+num=list(range(10,50,5))
+print(num)
+friends=['Alex','Davis','Abith']
+print(friends[2])
+for i in [1,2,3,4,5,6]:
+    print(i)
+print("Alex")
+'''
+'''
+greet="Hello alex"
+print(len(greet))
+num=[1,2,3,'alex',10]
+print(len(num))
+num_1=['alex']
+print(len(num_1))
+'''
+'''
+friends=["Alex","luke"]
+for friend in friends:
+    print("Happy new year: ",friend)
+'''
+'''
+even=[2,4,6,8]
+odd=[1,2,3,4,5]
+sum=[2,4,6,8]
+dd=[1,2,3,4,5]
+print(even==odd)
+print(sum==even)
+print(odd==dd)
+'''
+
+
